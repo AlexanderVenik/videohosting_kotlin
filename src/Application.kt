a98ab80e00b6a8a2075276f4d2f2bf75169b7f17
Alexander <81717191+AlexanderVenik@users.noreply.github.com>
2021-12-01 20:57:25 +0500

    add Mustache﻿

--- src/Application.kt
+++ src/Application.kt
@@ -1,11 +1,19 @@
 package com.example
 
+import com.github.mustachejava.DefaultMustacheFactory
+import io.ktor.application.install
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
+import io.ktor.features.*
+import io.ktor.mustache.*
 
 fun main() {
-    embeddedServer(Netty, port = 21764, host = "0.0.0.0") {
+    embeddedServer(Netty, port = 4587, host = "0.0.0.0") {
         configureRouting()
+        StaticContent()
+        install(Mustache) {
+            mustacheFactory = DefaultMustacheFactory("templates")
+        }
     }.start(wait = true)
 }
 
--- src/Routing.kt
+++ src/Routing.kt
@@ -3,14 +3,44 @@ package com.example
 import io.ktor.routing.*
 import io.ktor.http.*
 import io.ktor.application.*
+import io.ktor.http.content.file
+import io.ktor.http.content.static
+import io.ktor.mustache.MustacheContent
 import io.ktor.response.*
-import io.ktor.request.*
+import java.io.File
+
+data class User(val id: Int, val name: String)
 
 fun Application.configureRouting() {
-
     routing {
+        route("/")
+        {
+
+        }
         get("/") {
-            call.respondText("Hello World!")
+            call.respondText("Hello World kotlin!")
+        }
+        get("/file") {
+            try{
+                val html = File("./static/index.html").readText()
+                call.respondText(html, ContentType.Text.Html)
+            }
+            catch(e: Exception){
+                val err = e.message.toString()
+                call.respondText(err)
+            }
+        }
+        get("/index") {
+            val sampleUser = User(1, "John")
+            call.respond(MustacheContent("index.hbs", mapOf("user" to sampleUser)))
+        }
+    }
+}
+
+fun Application.StaticContent() {
+    routing {
+        static("static") {
+            file("index.html")
         }
     }
 }
